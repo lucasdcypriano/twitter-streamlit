f9be6c3a525adff348b2525e210f748ddade9558
luks5 <lucasdcypriano@gmail.com>
2022-08-23 16:57:57 -0300

    improved UI and added extra metrics to the data

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def twitterProfileScrape(twitter_url, n_tweets=100):
     user = twitter_url.replace('https://twitter.com/', '')
     # Using TwitterSearchScraper to scrape data and append tweets to list
     for i,tweet in enumerate(sntwitter.TwitterSearchScraper(f'from:{user}').get_items()):
-        if i>n_tweets:
+        if i > n_tweets:
             break
         
         else:
@@ -27,6 +27,10 @@ def twitterProfileScrape(twitter_url, n_tweets=100):
                                                     'Following Count', 'Datetime', 'Tweet Id', 'Text', 'Likes',
                                                      'Retweets', 'Replies'])
     
+    tweets_df['like_ratio'] = (tweets_df['Likes'] / tweets_df['Follower Count']) * 100
+    tweets_df['RT_ratio'] = (tweets_df['Retweets'] / tweets_df['Follower Count']) * 100
+    tweets_df['reply_ratio'] = (tweets_df['Replies'] / tweets_df['Follower Count']) * 100
+    tweets_df['engagement_ratio'] = ((tweets_df['Likes'] + tweets_df['Retweets'] + tweets_df['Replies']) / tweets_df['Follower Count']) * 100
     tweets_df['day'] = tweets_df.Datetime.dt.day
     tweets_df['week'] = tweets_df.Datetime.dt.isocalendar().week
     tweets_df['month'] = tweets_df.Datetime.dt.month
@@ -34,6 +38,7 @@ def twitterProfileScrape(twitter_url, n_tweets=100):
 
     return tweets_df
 
+@st.experimental_memo
 def twitterDataframeConcat(url_list, n_tweets):
     dataframe = pd.DataFrame(columns=['Username', 'Displayname', 'Description', 'Follower Count', 
                                       'Following Count', 'Datetime', 'Tweet Id', 'Text', 'Likes',
@@ -46,7 +51,7 @@ def twitterDataframeConcat(url_list, n_tweets):
 ########################### Data viz ###########################
 def make_chart(df, kind, period='week'):
     chart = (
-        alt.Chart(df.groupby(['Username', f'{period}'])[f'{kind}'].sum().rolling(4).mean().reset_index(), title=f'Nº {kind}')
+        alt.Chart(df.groupby(['Username', f'{period}'])[f'{kind}'].sum().rolling(4).mean().reset_index(), title=f'{kind}')
         .mark_area(opacity=0.3)
         .encode(
             x=f'{period}:T',
@@ -64,6 +69,10 @@ def handle_load_tweets(candidates, n_tweets):
         return df
 
 def handle_show_analytics(df, period):
+    st.altair_chart(make_chart(df=df, kind='engagement_ratio', period=period), use_container_width=True)
+    st.altair_chart(make_chart(df=df, kind='like_ratio', period=period), use_container_width=True)
+    st.altair_chart(make_chart(df=df, kind='RT_ratio', period=period), use_container_width=True)
+    st.altair_chart(make_chart(df=df, kind='reply_ratio', period=period), use_container_width=True)
     st.altair_chart(make_chart(df=df, kind='Likes', period=period), use_container_width=True)
     st.altair_chart(make_chart(df=df, kind='Retweets', period=period), use_container_width=True)
     st.altair_chart(make_chart(df=df, kind='Replies', period=period), use_container_width=True)
@@ -71,18 +80,23 @@ def handle_show_analytics(df, period):
 ###########################  UI  ###########################
 
 
-candidates = st.multiselect(
+candidates = st.sidebar.multiselect(
     label = 'Escolha os candidatos a serem analisados',
     options = ['https://twitter.com/jairbolsonaro', 'https://twitter.com/LulaOficial',
     'https://twitter.com/cirogomes','https://twitter.com/simonetebetbr', 
     'https://twitter.com/fdavilaoficial', 'https://twitter.com/verapstu',
     'https://twitter.com/pablomarcal'],
-    default = ['https://twitter.com/jairbolsonaro', 'https://twitter.com/LulaOficial'],
+    default = ['https://twitter.com/jairbolsonaro', 'https://twitter.com/LulaOficial', 
+               'https://twitter.com/cirogomes'],
     format_func = lambda any : re.sub('https://twitter.com/', '', any)
 )
 
-n_tweets = st.slider('Escolha o número de tweets a serem analisados', 100, 4000)
-period = st.radio('Escolha o intervalo', ('week', 'month'))
+period = st.sidebar.radio('Escolha o intervalo de agregação', ('date','week', 'month'))
+
+if period == 'week':
+    n_tweets = st.sidebar.slider('Escolha o número de tweets a serem analisados', 100, 4000)
+else:
+    n_tweets = st.sidebar.slider('Escolha o número de tweets a serem analisados', 1000, 5000)
 
 if st.button('Carregar tweets'):
     st.session_state['df'] = handle_load_tweets(candidates, n_tweets)
